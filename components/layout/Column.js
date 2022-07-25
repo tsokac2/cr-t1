1fe5c6c62e315a857c223e60728464134eb6441a
Tomislav Sokač <tsokac@fb.com>
2022-07-25 15:35:49 +0100

    Test node-sass with chakra UI layout

---
+++ components/layout/Column.js
@@ -0,0 +1,11 @@
+
+
+const Column = (props) => {
+    return (
+        <div>
+            {props.children}
+        </div>
+    )
+};
+
+export default Column;
--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,7 +1,7 @@
 // main layout componet, use SASS and Chakrea UI to modifi the main wrapre layout
 const Layout = (props) => {
   return (
-    <div className="test">
+    <div className="main-layout">
       <main>{props.children}</main>
     </div>
   );
